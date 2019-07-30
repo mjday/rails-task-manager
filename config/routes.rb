31d9f3842076c59c4677c0ddf31017544dec724c
Michael Day <mjd.poindexter@gmail.com>
2019-07-30 19:00:20 +0800

    adding bootstrap to the file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :tasks
+  # root "tasks#index"
   # get "tasks", to: "tasks#index", as: "tasks"
   # get "tasks/new", to: "tasks#new", as: "new_task"
   # get "tasks/:id", to: "tasks#show", as: "task"
